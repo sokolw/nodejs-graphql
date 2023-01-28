ac5daadd69771e28f9955dfadd9a6a7460f9b579
sokolw <sokolw21@gmail.com>
2023-01-29 00:57:10 +0300

    refactor: change inject to db in dataLoaders

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,14 +1,10 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
 import { graphqlBodySchema } from './schema';
 import { graphql, parse } from 'graphql';
-import * as DataLoader from 'dataloader';
 import { validate } from 'graphql/validation';
 import * as depthLimit from 'graphql-depth-limit';
 import { gqlSchema } from './schema-gql/index';
-import { PostEntity } from '../../utils/DB/entities/DBPosts';
-import { ProfileEntity } from '../../utils/DB/entities/DBProfiles';
-import { MemberTypeEntity } from '../../utils/DB/entities/DBMemberTypes';
-import { UserEntity } from '../../utils/DB/entities/DBUsers';
+import { connectDataLoaders } from './data-loaders';
 
 const DEFAULT_DEPTH_LIMIT = 6;
 
@@ -21,7 +17,7 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (fastify): Promise<void> 
       },
     },
     async function (request, reply) {
-      const gqlRequest = request.body!;
+      const { body: gqlRequest } = request;
       console.log(gqlRequest);
       if (gqlRequest.query || gqlRequest.mutation) {
         return graphql({
@@ -32,47 +28,7 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (fastify): Promise<void> 
             validationDepth: validate(gqlSchema, parse((gqlRequest.query || gqlRequest.mutation)!), [
               depthLimit(DEFAULT_DEPTH_LIMIT),
             ]),
-            dataLoader: {
-              postsLoader: new DataLoader(async (ids: readonly string[]) => {
-                const postsReq = await fastify.inject({
-                  method: 'GET',
-                  url: '/posts',
-                });
-                const posts = postsReq.json<PostEntity[]>();
-
-                return ids.map((id) => posts.filter((post) => post.userId === id));
-              }),
-
-              profilesLoader: new DataLoader(async (ids: readonly string[]) => {
-                const profilesReq = await fastify.inject({
-                  method: 'GET',
-                  url: `/profiles`,
-                });
-                const profiles = profilesReq.json<ProfileEntity[]>();
-
-                return ids.map((id) => profiles.filter((profile) => profile.userId === id));
-              }),
-
-              memberTypesLoader: new DataLoader(async (ids: readonly string[]) => {
-                const memberTypesReq = await fastify.inject({
-                  method: 'GET',
-                  url: `/member-types`,
-                });
-                const memberTypes = memberTypesReq.json<MemberTypeEntity[]>();
-
-                return ids.map((id) => memberTypes.filter((memberType) => memberType.id === id));
-              }),
-
-              usersLoader: new DataLoader(async (calls: readonly string[]) => {
-                const usersReq = await fastify.inject({
-                  method: 'GET',
-                  url: '/users',
-                });
-                const users = usersReq.json<UserEntity[]>();
-
-                return calls.map(() => users);
-              }),
-            },
+            dataLoader: connectDataLoaders(fastify),
           },
           variableValues: gqlRequest.variables,
         });
